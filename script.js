e101ab20d09ccc3c474fb01e92037fe613d1106a
Ian M. Nava Garduno <ianmng1@gmail.com>
2024-04-23 18:59:07 -0600

    Working with local storage

--- script.js
+++ script.js
@@ -74,12 +74,15 @@ class App {
   #workouts = [];
 
   constructor() {
+    // Get position
     this._getPosition();
 
+    // Get data from local storage
+    this._getLocalStorage();
+
+    // Event handlers
     form.addEventListener('submit', this._newWorkout.bind(this));
-
     inputType.addEventListener('change', this._toggleElevationField);
-
     containerWorkouts.addEventListener('click', this._moveToPopup.bind(this));
   }
 
@@ -108,6 +111,11 @@ class App {
     }).addTo(this.#map);
 
     this.#map.on('click', this._showForm.bind(this));
+
+    // Render workout markers from local storage
+    this.#workouts.forEach(work => {
+      this._renderWorkoutMarker(work);
+    });
   }
 
   _showForm(mapE) {
@@ -196,6 +204,9 @@ class App {
 
     // Hide form and clear fields
     this._hideForm();
+
+    // Set local storage to all workouts
+    this._setLocalStorage();
   }
 
   _renderWorkout(workout) {
@@ -279,8 +290,28 @@ class App {
       duration: 1,
     });
 
-    workout.click();
-    console.log(workout.clicks);
+    // workout.click();
+  }
+
+  _setLocalStorage() {
+    localStorage.setItem('workouts', JSON.stringify(this.#workouts));
+  }
+
+  _getLocalStorage() {
+    const data = JSON.parse(localStorage.getItem('workouts'));
+
+    if (!data) return;
+
+    this.#workouts = data;
+
+    this.#workouts.forEach(work => {
+      this._renderWorkout(work);
+    });
+  }
+
+  reset() {
+    localStorage.removeItem('workouts');
+    location.reload();
   }
 }
 
