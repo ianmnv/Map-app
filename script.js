c97ebacb64398e3ccb952d14ec655b866a36bf50
Ian M. Nava Garduno <ianmng1@gmail.com>
2024-04-29 14:23:31 -0600

    Fix small bug when editing a workout

--- script.js
+++ script.js
@@ -252,7 +252,9 @@ class App {
       html += `
         <div class="workout__details">
           <span class="workout__icon">⚡️</span>
-          <span class="workout__value">${workout.pace.toFixed(1)}</span>
+          <span class="workout__value">${
+            workout.pace ? workout.pace.toFixed(1) : 0
+          }</span>
           <span class="workout__unit">min/km</span>
         </div>
         <div class="workout__details">
@@ -266,7 +268,9 @@ class App {
       html += `
         <div class="workout__details">
           <span class="workout__icon">⚡️</span>
-          <span class="workout__value">${workout.speed.toFixed(1)}</span>
+          <span class="workout__value">${
+            workout.speed ? workout.speed.toFixed(1) : 0
+          }</span>
           <span class="workout__unit">km/h</span>
         </div>
         <div class="workout__details">
@@ -514,6 +518,7 @@ class App {
     if (e.target.classList.contains('show_btn')) {
     }
 
+    // Sorting workouts
     if (e.target.classList.contains('sort_select')) {
       const selectedOpt = e.target.value;
 
